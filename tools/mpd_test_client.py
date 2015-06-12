bfc816610c5b6070209ab83958dab1f29abab48c
Varun Agrawal <varagrawal@gmail.com>
2015-06-12 21:00:00 +0530

    updated client test tool

--- tools/mpd_test_client.py
+++ tools/mpd_test_client.py
@@ -7,19 +7,18 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 
 while True:
+
+	MESSAGE = input("$ ")
+
+	if MESSAGE == "exit":
+		break
+
+	s.send((MESSAGE + "\n").encode('ascii'))
+
 	data = s.recv(1024)
 	result = data.decode('ascii') #.split('\n')
 	print(result)
-	
+
 	#print repr(data)
-	
-	MESSAGE = input("$ ")
-	
-	if MESSAGE == "exit":
-		break
-		
-	s.send((MESSAGE + "\n").encode('ascii'))
-	
-	
+
 s.close()
-	
