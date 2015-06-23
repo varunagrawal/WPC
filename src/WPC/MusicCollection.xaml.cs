3d28093b1fb0553af6bfd6f1c344f2cc51543c00
Varun Agrawal <varagrawal@gmail.com>
2015-06-23 22:41:36 +0530

    Deserialize library response from MPD
    2 step process: Get all files and then get each file's info.

--- src/WPC/MusicCollection.xaml.cs
+++ src/WPC/MusicCollection.xaml.cs
@@ -65,16 +65,77 @@ namespace WPC
 			SocketClient client = new SocketClient();
 			string response = await client.Command("listall");
 
-			ObservableCollection<Song> library = FormatLibrary(response);
+			ObservableCollection<Song> library = await FormatLibrary(response);
 
 			return library;
 		}
 
-		private ObservableCollection<Song> FormatLibrary(string value)
+		private async Task<ObservableCollection<Song>> FormatLibrary(string value)
 		{
 			ObservableCollection<Song> library = new ObservableCollection<Song>();
 
+			List<string> values = new List<string>(value.Split('\n'));
+
+			foreach(string v in values)
+			{
+				if(v.Contains("file"))
+				{
+					Song s = await GetSongDetails(v.Substring(6));
+					library.Add(s);
+				}
+			}
+
+			
+
 			return library;
+		}
+
+		private async Task<Song> GetSongDetails(string file)
+		{
+			SocketClient client = new SocketClient();
+			string response = await client.Command(string.Format(@"find file ""{0}""", file));
+
+			string[] details = response.Split('\n');
+
+			Song s = new Song();
+
+			foreach(string t in details)
+			{
+				if(t.Contains("file"))
+				{
+					s.Filename = t.Substring("file: ".Length);
+				}
+				else if (t.Contains("Time")) 
+				{
+					s.Time = long.Parse(t.Substring("Time: ".Length));
+				}
+				else if (t.Contains("Artist")) 
+				{
+					s.Artist = t.Substring("Artist: ".Length);
+				}
+				else if (t.Contains("Title")) 
+				{
+					s.Title = t.Substring("Title: ".Length);
+				}
+				else if (t.Contains("Album")) 
+				{
+					s.Album = t.Substring("Album: ".Length);
+				}
+				else if (t.Contains("Track")) 
+				{
+					s.Track = t.Substring("Track: ".Length);
+				}
+				else if (t.Contains("Date")) 
+				{
+					s.Date = t.Substring("Date: ".Length);
+				}
+				else if (t.Contains("Genre")) 
+				{
+					s.Genre = t.Substring("Genre: ".Length);
+				}
+			}
+
+			return s;
 		}
 	}
 }
